4c7adaa7772fb4d7db5775f3f322b5e0cc2e63ab
k5va <k5va@yandex.ru>
2019-06-22 09:55:50 +0300

    Changed getRandomElementFromArray function return value type in jsDocs

--- js/main.js
+++ js/main.js
@@ -28,7 +28,7 @@ var MAP_FILTERS_FORM_FIELDS = MAP_FILTERS_FORM.querySelectorAll('fieldset, selec
 /**
  * Returns random element of an array
  * @param {Array} elements - array of some elements
- * @return {object} - random element of the given array
+ * @return {*} - random element of the given array
  */
 var getRandomElementFromArray = function (elements) {
   return elements[Math.floor(Math.random() * elements.length)];
