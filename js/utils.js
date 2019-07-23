934eda986a6aefaa51910eaeb6713a33301d6ac0
k5va <k5va@yandex.ru>
2019-07-23 09:49:18 +0300

    Removed unused methods in utils

--- js/utils.js
+++ js/utils.js
@@ -4,25 +4,6 @@
   var DEBOUNCE_INTERVAL = 500; // ms
 
   /**
-   * Returns random element of an array
-   * @param {Array} elements - array of some elements
-   * @return {*} - random element of the given array
-   */
-  var getRandomElementFromArray = function (elements) {
-    return elements[Math.floor(Math.random() * elements.length)];
-  };
-
-  /**
-   * Returns random number form the given range
-   * @param {number} min - minimum of the range
-   * @param {number} max - maximum of the range
-   * @return {number} - random number from the given range
-   */
-  var generateRandomNumberFromRange = function (min, max) {
-    return min + Math.floor(Math.random() * (max - min));
-  };
-
-  /**
    * Debounces given function - returns new function, that calls callback only after DEBOUNCE_INTERVAL is passed
    * @param {function} callback - callback function
    * @return {function} - debounced function
@@ -44,8 +25,6 @@
   };
 
   window.utils = {
-    getRandomElementFromArray: getRandomElementFromArray,
-    generateRandomNumberFromRange: generateRandomNumberFromRange,
     debounce: debounce
   };
 })();
